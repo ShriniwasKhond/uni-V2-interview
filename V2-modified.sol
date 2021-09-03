d612f563781e55ed6e2cb443e88919d562aa276e
Shriniwaskhond <68766398+ShriniwasKhond@users.noreply.github.com>
2021-09-03 20:09:31 +0530

    Update V2-modified.sol

--- V2-modified.sol
+++ V2-modified.sol
@@ -1,181 +1,201 @@
-// SPDX-License-Identifier: GPL-3.0
+pragma solidity =0.5.16;
 
-pragma solidity >=0.7.0 <0.9.0;
-import "./Pay.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "https://github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/interfaces/IUniswapV2Router02.sol";
-interface IUniswapV2Router {
-  function getAmountsOut(uint256 amountIn, address[] memory path)
-    external
-    view
-    returns (uint256[] memory amounts);
-  
-  function swapExactTokensForTokens(
-  
-    //amount of tokens we are sending in
-    uint256 amountIn,
-    //the minimum amount of tokens we want out of the trade
-    uint256 amountOutMin,
-    //list of token addresses we are going to trade in.  this is necessary to calculate amounts
-    address[] calldata path,
-    //this is the address we are going to send the output tokens to
-    address to,
-    //the last time that the trade is valid for
-    uint256 deadline
-  ) external returns (uint256[] memory amounts);
-}
+import 'https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/interfaces/IUniswapV2Pair.sol';
+import 'https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/UniswapV2ERC20.sol';
+import 'https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/libraries/Math.sol';
+import 'https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/libraries/UQ112x112.sol';
+import 'https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/interfaces/IERC20.sol';
+import 'https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/interfaces/IUniswapV2Factory.sol';
+import 'https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/interfaces/IUniswapV2Callee.sol';
 
-interface IUniswapV2Pair {
-  function token0() external view returns (address);
-  function token1() external view returns (address);
-  function swap(
-    uint256 amount0Out,
-    uint256 amount1Out,
-    address to,
-    bytes calldata data
-  ) external;
-}
+contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
+    using SafeMath  for uint;
+    using UQ112x112 for uint224;
 
-interface IUniswapV2Factory {
-  function getPair(address token0, address token1) external returns (address);
-}
+    uint public constant MINIMUM_LIQUIDITY = 10**3;
+    bytes4 private constant SELECTOR = bytes4(keccak256(bytes('transfer(address,uint256)')));
 
+    address public factory;
+    address public token0;
+    address public token1;
 
+    uint112 private reserve0;           // uses single storage slot, accessible via getReserves
+    uint112 private reserve1;           // uses single storage slot, accessible via getReserves
+    uint32  private blockTimestampLast; // uses single storage slot, accessible via getReserves
 
-contract tokenSwap {
-    
-    //address of the uniswap v2 router
-    address private constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-    
-    //address of WETH token.  This is needed because some times it is better to trade through WETH.  
-    //you might get a better price using WETH.  
-    //example trading from token A to WETH then WETH to token B might result in a better price
-    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    
+    uint public price0CumulativeLast;
+    uint public price1CumulativeLast;
+    uint public kLast; // reserve0 * reserve1, as of immediately after the most recent liquidity event
 
-    //this swap function is used to trade from one token to another
-    //the inputs are self explainatory
-    //token in = the token address you want to trade out of
-    //token out = the token address you want as the output of this trade
-    //amount in = the amount of tokens you are sending in
-    //amount out Min = the minimum amount of tokens you want out of the trade
-    //to = the address you want the tokens to be sent to
-    
-   function swap(address _tokenIn, address _tokenOut, uint256 _amountIn, uint256 _amountOutMin, address _to) external {
-      
-    //first we need to transfer the amount in tokens from the msg.sender to this contract
-    //this contract will have the amount of in tokens
-    IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
-    
-    //next we need to allow the uniswapv2 router to spend the token we just sent to this contract
-    //by calling IERC20 approve you allow the uniswap contract to spend the tokens in this contract 
-    IERC20(_tokenIn).approve(UNISWAP_V2_ROUTER, _amountIn);
-
-    //path is an array of addresses.
-    //this path array will have 3 addresses [tokenIn, WETH, tokenOut]
-    //the if statement below takes into account if token in or token out is WETH.  then the path is only 2 addresses
-    address[] memory path;
-    if (_tokenIn == WETH || _tokenOut == WETH) {
-      path = new address[](2);
-      path[0] = _tokenIn;
-      path[1] = _tokenOut;
-    } else {
-      path = new address[](3);
-      path[0] = _tokenIn;
-      path[1] = WETH;
-      path[2] = _tokenOut;
-    }
-        //then we will call swapExactTokensForTokens
-        //for the deadline we will pass in block.timestamp
-        //the deadline is the latest time the trade is valid for
-        IUniswapV2Router(UNISWAP_V2_ROUTER).swapExactTokensForTokens(_amountIn, _amountOutMin, path, _to, block.timestamp);
-    }
-    
-       //this function will return the minimum amount from a swap
-       //input the 3 parameters below and it will return the minimum amount out
-       //this is needed for the swap function above
-     function getAmountOutMin(address _tokenIn, address _tokenOut, uint256 _amountIn) external view returns (uint256) {
-
-       //path is an array of addresses.
-       //this path array will have 3 addresses [tokenIn, WETH, tokenOut]
-       //the if statement below takes into account if token in or token out is WETH.  then the path is only 2 addresses
-        address[] memory path;
-        if (_tokenIn == WETH || _tokenOut == WETH) {
-            path = new address[](2);
-            path[0] = _tokenIn;
-            path[1] = _tokenOut;
-        } else {
-            path = new address[](3);
-            path[0] = _tokenIn;
-            path[1] = WETH;
-            path[2] = _tokenOut;
-        }
-        
-        uint256[] memory amountOutMins = IUniswapV2Router(UNISWAP_V2_ROUTER).getAmountsOut(_amountIn, path);
-        return amountOutMins[path.length -1];  
-    }  
-}
-contract Market is tokenSwap{
-    using SafeMath for uint;
-    struct Asset{
-        uint id;
-        uint vendor_id;
-        uint price;
-    }
-    event listAsset(uint id, uint price);
-    Pay token;
-    tokenSwap D;
-    address internal constant UNISWAP_ROUTER_ADDRESS = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D; //ropsten adddress
-    IUniswapV2Router02 public uniswapRouter = IUniswapV2Router02(UNISWAP_ROUTER_ADDRESS);
-    address private pay = 0xEb306583b6ce2b4559740F822272d3E85bf67777; //ropsten address
-    address private usdt = 0xAa0dcE5ec02e54F9FA368Fc50F3bEa9aef8F52c4; //ropsten address
-    event feeChange(uint id, uint fee);
-    struct Vendor{
-        string name;
-        uint id;
-        address vendor_ad;
-        uint platform_fee;
-    }
-    mapping(address => Vendor) public vendor;
-    mapping(address => Asset) public order;
-    Vendor [] public vendors;
-    Asset [] public assets;
-    modifier onlyVendor{
-        require(vendor[msg.sender].id != 0, "Only vendors can change fee");
+    uint private unlocked = 1;
+    modifier lock() {
+        require(unlocked == 1, 'UniswapV2: LOCKED');
+        unlocked = 0;
         _;
+        unlocked = 1;
     }
-    constructor(string[] memory names, address[] memory _vendors, uint[] memory fee){
-        for(uint i = 0; i <vendors.length; i++ ){
-            vendors.push(Vendor({
-                name: names[i],
-                id: i++,
-                vendor_ad: _vendors[i],
-                platform_fee: fee[i]
-            }));
+
+    function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) {
+        _reserve0 = reserve0;
+        _reserve1 = reserve1;
+        _blockTimestampLast = blockTimestampLast;
+    }
+
+    function _safeTransfer(address token, address to, uint value) private {
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(SELECTOR, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'UniswapV2: TRANSFER_FAILED');
+    }
+
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+    constructor() public {
+        factory = msg.sender;
+    }
+
+    // called once by the factory at time of deployment
+    function initialize(address _token0, address _token1) external {
+        require(msg.sender == factory, 'UniswapV2: FORBIDDEN'); // sufficient check
+        token0 = _token0;
+        token1 = _token1;
+    }
+
+    // update reserves and, on the first call per block, price accumulators
+    function _update(uint balance0, uint balance1, uint112 _reserve0, uint112 _reserve1) private {
+        require(balance0 <= uint112(-1) && balance1 <= uint112(-1), 'UniswapV2: OVERFLOW');
+        uint32 blockTimestamp = uint32(block.timestamp % 2**32);
+        uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
+        if (timeElapsed > 0 && _reserve0 != 0 && _reserve1 != 0) {
+            // * never overflows, and + overflow is desired
+            price0CumulativeLast += uint(UQ112x112.encode(_reserve1).uqdiv(_reserve0)) * timeElapsed;
+            price1CumulativeLast += uint(UQ112x112.encode(_reserve0).uqdiv(_reserve1)) * timeElapsed;
+        }
+        reserve0 = uint112(balance0);
+        reserve1 = uint112(balance1);
+        blockTimestampLast = blockTimestamp;
+        emit Sync(reserve0, reserve1);
+    }
+
+    // if fee is on, mint liquidity equivalent to 1/6th of the growth in sqrt(k)
+    function _mintFee(uint112 _reserve0, uint112 _reserve1) private returns (bool feeOn) {
+        address feeTo = IUniswapV2Factory(factory).feeTo();
+        feeOn = feeTo != address(0);
+        uint _kLast = kLast; // gas savings
+        if (feeOn) {
+            if (_kLast != 0) {
+                uint rootK = Math.sqrt(uint(_reserve0).mul(_reserve1));
+                uint rootKLast = Math.sqrt(_kLast);
+                if (rootK > rootKLast) {
+                    uint numerator = totalSupply.mul(rootK.sub(rootKLast));
+                    uint denominator = rootK.mul(5).add(rootKLast);
+                    uint liquidity = (numerator.add(denominator))/2;
+                    if (liquidity > 0) _mint(feeTo, liquidity);
+                }
+            }
+        } else if (_kLast != 0) {
+            kLast = 0;
         }
     }
-    function changeFee(uint id,uint fee) onlyVendor public{
-        vendors[id].platform_fee = fee;
-        emit feeChange(id, fee);
+
+    // this low-level function should be called from a contract which performs important safety checks
+    function mint(address to) external lock returns (uint liquidity) {
+        (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
+        uint balance0 = IERC20(token0).balanceOf(address(this));
+        uint balance1 = IERC20(token1).balanceOf(address(this));
+        uint amount0 = balance0.sub(_reserve0);
+        uint amount1 = balance1.sub(_reserve1);
+
+        bool feeOn = _mintFee(_reserve0, _reserve1);
+        uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
+        if (_totalSupply == 0) {
+            liquidity = Math.sqrt(amount0.mul(amount1)).sub(MINIMUM_LIQUIDITY);
+           _mint(address(0), MINIMUM_LIQUIDITY); // permanently lock the first MINIMUM_LIQUIDITY tokens
+        } else {
+            liquidity = Math.min(amount0.mul(_totalSupply) / _reserve0, amount1.mul(_totalSupply) / _reserve1);
+        }
+        require(liquidity > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_MINTED');
+        _mint(to, liquidity);
+
+        _update(balance0, balance1, _reserve0, _reserve1);
+        if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date
+        emit Mint(msg.sender, amount0, amount1);
     }
-    function addAsset(uint _id, uint _vendor_id, uint _price) public{
-        assets.push(Asset({
-            id: _id,
-            vendor_id: _vendor_id ,
-            price: _price
-        }));
-        emit listAsset(_id, _price);
+
+    // this low-level function should be called from a contract which performs important safety checks
+    function burn(address to) external lock returns (uint amount0, uint amount1) {
+        (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
+        address _token0 = token0;                                // gas savings
+        address _token1 = token1;                                // gas savings
+        uint balance0 = IERC20(_token0).balanceOf(address(this));
+        uint balance1 = IERC20(_token1).balanceOf(address(this));
+        uint liquidity = balanceOf[address(this)];
+
+        bool feeOn = _mintFee(_reserve0, _reserve1);
+        uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
+        amount0 = liquidity.mul(balance0) / _totalSupply; // using balances ensures pro-rata distribution
+        amount1 = liquidity.mul(balance1) / _totalSupply; // using balances ensures pro-rata distribution
+        require(amount0 > 0 && amount1 > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_BURNED');
+        _burn(address(this), liquidity);
+        _safeTransfer(_token0, to, amount0);
+        _safeTransfer(_token1, to, amount1);
+        balance0 = IERC20(_token0).balanceOf(address(this));
+        balance1 = IERC20(_token1).balanceOf(address(this));
+
+        _update(balance0, balance1, _reserve0, _reserve1);
+        if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date
+        emit Burn(msg.sender, amount0, amount1, to);
     }
-    function buyToken(uint _amount) public payable returns(bool success){
-        token.transfer(msg.sender, _amount);
-        return true;
+
+    // this low-level function should be called from a contract which performs important safety checks
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
+        require(amount0Out > 0 || amount1Out > 0, 'UniswapV2: INSUFFICIENT_OUTPUT_AMOUNT');
+        (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
+        require(amount0Out < _reserve0 && amount1Out < _reserve1, 'UniswapV2: INSUFFICIENT_LIQUIDITY');
+
+        uint balance0;
+        uint balance1;
+        { // scope for _token{0,1}, avoids stack too deep errors
+        address _token0 = token0;
+        address _token1 = token1;
+        require(to != _token0 && to != _token1, 'UniswapV2: INVALID_TO');
+        if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
+        if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
+        if (data.length > 0) IUniswapV2Callee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
+        balance0 = IERC20(_token0).balanceOf(address(this));
+        balance1 = IERC20(_token1).balanceOf(address(this));
+        }
+        uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
+        uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
+        require(amount0In > 0 || amount1In > 0, 'UniswapV2: INSUFFICIENT_INPUT_AMOUNT');
+        { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
+        uint balance0Adjusted = balance0.mul(1000).sub(amount0In.mul(3));
+        uint balance1Adjusted = balance1.mul(1000).sub(amount1In.mul(3));
+        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'UniswapV2: K');
+        }
+
+        _update(balance0, balance1, _reserve0, _reserve1);
+        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
     }
-    function buyAsset(uint _id) external payable{
-        order[msg.sender] = assets[_id];
-        uint k = assets[_id].vendor_id;
-        uint platformFee = assets[_id].price.mul(vendors[k].platform_fee).div(100);
-        token.transferFrom(msg.sender, address(this), platformFee);
-        uint amountOutMin = D.getAmountOutMin(pay, usdt, assets[_id].price.sub(platformFee));
-        D.swap(pay,usdt, assets[_id].price.sub(platformFee), amountOutMin, vendors[k].vendor_ad);
+
+    // force balances to match reserves
+    function skim(address to) external lock {
+        address _token0 = token0; // gas savings
+        address _token1 = token1; // gas savings
+        _safeTransfer(_token0, to, IERC20(_token0).balanceOf(address(this)).sub(reserve0));
+        _safeTransfer(_token1, to, IERC20(_token1).balanceOf(address(this)).sub(reserve1));
+    }
+
+    // force reserves to match balances
+    function sync() external lock {
+        _update(IERC20(token0).balanceOf(address(this)), IERC20(token1).balanceOf(address(this)), reserve0, reserve1);
     }
 }
